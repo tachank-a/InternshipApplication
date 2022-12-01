2a7ec1c2ab25a17f5725035f84f2bd99a3ab1c56
tachank-a <gildo2015@yandex.ru>
2022-12-02 00:50:25 +0700

    Initial commit

---
+++ src/main/java/ru/gildo/application/InternshipApplication.java
@@ -0,0 +1,13 @@
+package ru.gildo.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipApplication.class, args);
+    }
+
+}
