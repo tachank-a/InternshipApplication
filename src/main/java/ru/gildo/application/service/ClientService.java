63a83e35714eb466db818d99fd162066f80c155f
tachank-a <gildo2015@yandex.ru>
2022-12-04 16:41:34 +0700

    Change return type to Optional<ClientDTO>

--- src/main/java/ru/gildo/application/service/ClientService.java
+++ src/main/java/ru/gildo/application/service/ClientService.java
@@ -3,10 +3,11 @@ package ru.gildo.application.service;
 import ru.gildo.application.model.dto.ClientDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClientService {
 
     List<ClientDTO> getAllClients();
 
-    ClientDTO getClientById(long id);
+    Optional<ClientDTO> getClientById(long id);
 }
