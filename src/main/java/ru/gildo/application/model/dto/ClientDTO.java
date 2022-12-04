4279e3a3592a2c0b39f35b2463ecacd3c9d71dfe
tachank-a <gildo2015@yandex.ru>
2022-12-04 16:40:01 +0700

    Add AllArgsConstructor annotation

--- src/main/java/ru/gildo/application/model/dto/ClientDTO.java
+++ src/main/java/ru/gildo/application/model/dto/ClientDTO.java
@@ -1,19 +1,17 @@
 package ru.gildo.application.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-import ru.gildo.application.model.entity.Account;
 
-import java.util.List;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class ClientDTO {
     long id;
     String firstName;
     String lastName;
     String patronymic;
-    String secretWord;
-    List<Account> accounts;
 }
